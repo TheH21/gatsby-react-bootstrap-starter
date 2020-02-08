bf618241c18169f806f734122c8a694b15d75706
Davido <htwenty1@gmail.com>
2020-02-07 19:51:11 -0800

    Man I'm tryin over here

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -12,7 +12,7 @@ const CustomNavbar = ({ pageInfo }) => {
     <Link to="/">
     <img src={logo} id="logo" alt=""></img>
     </Link>
-    <div class="float-right">
+    <div class="d-flex justify-content-center">
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
@@ -40,7 +40,33 @@ const CustomNavbar = ({ pageInfo }) => {
     </div>
       <Container id="whiteBox" class="fixed-bottom">
           <Container id="navCategs">
-            <p>Hey</p>
+
+          <div class="d-flex justify-content-center">
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav className="mr-auto" activeKey={pageInfo && pageInfo.pageName}>
+            <Link to="/page-2" className="link-no-style">
+              <Nav.Link as="span" eventKey="page-2">
+                Page 2
+              </Nav.Link>
+            </Link>
+          </Nav>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+    </div>
           </Container>
       </Container>
     </Container>
